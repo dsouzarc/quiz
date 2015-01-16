f4f961a435d3ed71098aec4697085cea2f8db36a
Ryan D'souza <dsouzarc@gmail.com>
2015-01-16 12:02:02 -0500

    Controller class --> 'C' of MVC design

---
+++ Quiz/BNRViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRViewController.h
+//  Quiz
+//
+//  Created by Ryan D'souza on 1/16/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRViewController : UIViewController
+
+@end
