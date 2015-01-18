a196a023c98a0854e17d073c396c0257661f6474
Ryan D'souza <dsouzarc@gmail.com>
2015-01-18 12:39:20 -0500

    Logic to display question and answer when clicked

--- Quiz/BNRViewController.m
+++ Quiz/BNRViewController.m
@@ -10,6 +10,11 @@
 
 @interface BNRViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic) NSArray *questions;
+@property (nonatomic) NSArray *answers;
+
 @property (nonatomic, weak) IBOutlet UILabel *questionLabel;
 @property (nonatomic, weak) IBOutlet UILabel *answerLabel;
 
@@ -17,13 +22,39 @@
 
 @implementation BNRViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if(self) {
+        self.questions = @[@"From what is cognac made?", @"What is 7 + 7", @"What is the capital of Vermont"];
+        self.answers = @[@"Grapes", @"14", @"Montpelier"];
+    }
+    
+    return self;
+}
+
 - (IBAction)showQuestion:(id)sender
 {
+    self.currentQuestionIndex++;
     
+    if(self.currentQuestionIndex == self.questions.count) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"?";
 }
 
 - (IBAction)showAnswer:(id)sender
 {
-
+    self.currentQuestionIndex++;
+    
+    if(self.currentQuestionIndex == self.questions.count) {
+        self.currentQuestionIndex = 0;
+    }
+    self.answerLabel.text = self.answers[self.currentQuestionIndex];
 }
+
 @end
